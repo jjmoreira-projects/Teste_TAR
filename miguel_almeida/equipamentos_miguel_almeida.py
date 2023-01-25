ab6a992c58c7aa2a450f2da810efb2735b61b04c
Miguel Cláudio Cardoso de Almeida <a22100636@alunos.local>
2023-01-25 11:33:51 +0000

    Teste de: miguel_almeida

---
+++ miguel_almeida/equipamentos_miguel_almeida.py
@@ -0,0 +1,29 @@
+x = 0
+y = 0
+ficheiro = open("devices.txt", "r")
+lista_equipamentos = []
+
+for linha in ficheiro:
+    linha = linha.strip()
+    lista_equipamentos.append(linha)
+
+ficheiro.close()
+
+for item in lista_equipamentos:
+        print(item)
+
+
+while x != 1:
+    nome_equipamento = input("Nome equipamento (sair p/terminar): ")
+    if nome_equipamento == "sair":
+        x=1
+        break
+    else:
+        for item in lista_equipamentos:
+            if item == nome_equipamento:
+                y = y + 1
+
+        if y != 0:
+            print("Existem "+ str(y) + " equipamentos que mencionaste")
+        else:
+            print("Equipamentos Não existe na Lista!")
